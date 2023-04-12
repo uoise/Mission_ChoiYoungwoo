9fed690ba16a80d0059edeb6d78663b84e7ffc93
Youngwoo Choi <yw823x@daum.net>
2023-04-12 15:28:26 +0900

    [update] OAuth2Attribute apply builder

--- src/main/java/com/ll/gramgram/base/security/entity/OAuth2Attribute.java
+++ src/main/java/com/ll/gramgram/base/security/entity/OAuth2Attribute.java
@@ -1,14 +1,17 @@
 package com.ll.gramgram.base.security.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 import java.util.Map;
 
+// OAuth2 Attribute DTO
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OAuth2Attribute {
     private String username;
     private String password;
@@ -16,11 +19,4 @@ public class OAuth2Attribute {
     private String profileImage;
     private Map<String, Object> attributes;
     private Collection<? extends GrantedAuthority> authorities;
-
-    public OAuth2Attribute(String username, String email, String profileImage, Map<String, Object> attributes) {
-        this.username = username;
-        this.email = email;
-        this.profileImage = profileImage;
-        this.attributes = attributes;
-    }
 }
